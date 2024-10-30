bc8fb0d3d38ba04ac61e1ad4975ef3826601a562
Maxi Broin <maxi.broin@newshore.es>
2024-10-29 21:17:21 -0300

    feat(home): add home page component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
