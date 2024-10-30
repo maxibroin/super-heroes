288a306cf3ca980a3d5d3cfdb21810288ef4fd5f
Maxi Broin <maxi.broin@newshore.es>
2024-10-29 21:41:50 -0300

    feat(routing): implement lazy loading for superhero list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { SuperHeroListComponent } from './pages/super-hero-list/super-hero-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'listado', component: SuperHeroListComponent },
+  {
+    path: 'listado',
+    loadComponent: () =>
+      import('./pages/super-hero-list/super-hero-list.component').then(
+        (m) => m.SuperHeroListComponent
+      ),
+  },
   { path: '**', redirectTo: '' },
 ];
